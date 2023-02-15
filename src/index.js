4e3b5e8117bedfe6dd16ecad8558fc20aa916c4a
fbuitrago <fabianbuitragov@gmail.com>
2023-02-15 10:12:33 -0500

    add first route and versioning

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const express = require("express");
+const v1Router = require("./v1/routes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/api/v1", v1Router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ src/v1/routes/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.route("/").get((req, res) => {
+  res.send(`<h1>Hello from ${req.baseUrl}</h1>`);
+});
+
+module.exports = router;
