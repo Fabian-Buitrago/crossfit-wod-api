65500514c421f386963189be0dcb80f5851dedd4
fbuitrago <fabianbuitragov@gmail.com>
2023-02-15 10:45:02 -0500

    Separate responsibilities

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,20 +1,33 @@
+// *** ADD ***
+const workoutService = require("../services/workoutService");
+
 const getAllWorkouts = (req, res) => {
+  // *** ADD ***
+  const allWorkouts = workoutService.getAllWorkouts();
   res.send("Get all workouts");
 };
 
 const getOneWorkout = (req, res) => {
+  // *** ADD ***
+  const workout = workoutService.getOneWorkout();
   res.send("Get an existing workout");
 };
 
 const createNewWorkout = (req, res) => {
+  // *** ADD ***
+  const createdWorkout = workoutService.createNewWorkout();
   res.send("Create a new workout");
 };
 
 const updateOneWorkout = (req, res) => {
+  // *** ADD ***
+  const updatedWorkout = workoutService.updateOneWorkout();
   res.send("Update an existing workout");
 };
 
 const deleteOneWorkout = (req, res) => {
+  // *** ADD ***
+  workoutService.deleteOneWorkout();
   res.send("Delete an existing workout");
 };
 
