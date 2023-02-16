d5c03221fa3d62cc77daeda73cbfa465d291d5ed
fbuitrago <fabianbuitragov@gmail.com>
2023-02-15 21:17:28 -0500

    Create workout endPoint

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,32 +1,45 @@
-// *** ADD ***
 const workoutService = require("../services/workoutService");
 
 const getAllWorkouts = (req, res) => {
-  // *** ADD ***
   const allWorkouts = workoutService.getAllWorkouts();
   res.send({ status: "OK", data: allWorkouts });
 };
 
 const getOneWorkout = (req, res) => {
-  // *** ADD ***
   const workout = workoutService.getOneWorkout();
   res.send("Get an existing workout");
 };
 
 const createNewWorkout = (req, res) => {
-  // *** ADD ***
-  const createdWorkout = workoutService.createNewWorkout();
-  res.send("Create a new workout");
+  const { body } = req;
+  if (
+    !body.name ||
+    !body.mode ||
+    !body.equipment ||
+    !body.exercises ||
+    !body.trainerTips
+  ) {
+    return;
+  }
+
+  const newWorkout = {
+    name: body.name,
+    mode: body.mode,
+    equipment: body.equipment,
+    exercises: body.exercises,
+    trainerTips: body.trainerTips,
+  };
+
+  const createdWorkout = workoutService.createNewWorkout(newWorkout);
+  res.status(201).send({ status: "OK", data: createdWorkout });
 };
 
 const updateOneWorkout = (req, res) => {
-  // *** ADD ***
   const updatedWorkout = workoutService.updateOneWorkout();
   res.send("Update an existing workout");
 };
 
 const deleteOneWorkout = (req, res) => {
-  // *** ADD ***
   workoutService.deleteOneWorkout();
   res.send("Delete an existing workout");
 };
