423ce25db2e519a739f8cdf344f0306a767e13f8
fbuitrago <fabianbuitragov@gmail.com>
2023-02-20 00:08:10 -0500

    create record db file

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -113,5 +113,4 @@ module.exports = {
   createNewWorkout,
   updateOneWorkout,
   deleteOneWorkout,
-  getRecordsForWorkout,
 };
--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const workoutController = require("../../controllers/workoutController");
+const recordController = require("../../controllers/recordController");
 
 const router = express.Router();
 
 router
   .get("/", workoutController.getAllWorkouts)
   .get("/:workoutId", workoutController.getOneWorkout)
+  .get("/:workoutId/records", recordController.getRecordForWorkout)
   .post("/", workoutController.createNewWorkout)
   .patch("/:workoutId", workoutController.updateOneWorkout)
   .delete("/:workoutId", workoutController.deleteOneWorkout);
