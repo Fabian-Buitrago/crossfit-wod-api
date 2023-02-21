8edbeeb71ea002b1d38113a5781c274cc3c744cd
fbuitrago <fabianbuitragov@gmail.com>
2023-02-21 16:30:44 -0500

    Create record controller

--- src/index.js
+++ src/index.js
@@ -9,5 +9,5 @@ app.use(express.json());
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`🚀 Server listening on port ${PORT}`);
 });
