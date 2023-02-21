39b81d19e7e35fb4c1cdf482104fd0d7f208e892
fbuitrago <fabianbuitragov@gmail.com>
2023-02-21 15:38:28 -0500

    use express parse

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
